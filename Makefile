5f7a195f07e0ab556977c63071bc3d9fdbc88ba0
Rafael Zarate <rafazrte@gmail.com>
2018-03-19 12:57:23 -0700

    yay

---
+++ Makefile
@@ -0,0 +1,37 @@
+MAKE = make -C
+NAME = fdf
+LIB = src/libft/
+CFLAGS = -Wall -Wextra
+CC = gcc
+
+#When compiling. you need to add your .a lib
+CFILES = $(LIB)/libft.a \
+        src/main.c \
+
+#These options are here in case the lib needs to be recompiled.
+#LIBM, LIBC, LIBF will run rules re, clean and fclean inside the libft folder
+LIBM = $(MAKE) $(LIB)
+LIBR = $(MAKE) $(LIB) re
+LIBC = $(MAKE) $(LIB) clean
+LIBF = $(MAKE) $(LIB) fclean
+OBJECTS = $(CFILES:.c=.o)
+
+all: $(NAME)
+
+$(NAME):
+	@$(LIBM)
+	@$(CC) $(CFLAGS) -I. -o $(NAME) $(CFILES)
+	@echo "FDF: Successfull compilation"
+	
+
+clean:
+	@$(LIBC)
+	@/bin/rm -f $(OBJECTS) printf:.o
+	@echo "FDF: Removed .o files"
+
+fclean:
+	@$(LIBF)
+	@/bin/rm -f $(OBJECTS) $(NAME)
+	@echo "FDF: Removed exacutable & .o files"
+
+re: fclean all
--- src/main.c
+++ src/main.c
@@ -6,49 +6,38 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 11:58:16 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/19 12:40:40 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/19 12:55:53 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-static	char	**parser(char *file)
+static	char	*reader(char *file)
 {
 	int		fd;
-	int		x;
 	char	*line;
-	char	*unparsed_input;
-	char	**input;
+	char	*input;
 	int		i;
+	int		i2;
 
+	i2 = -1;
 	i = -1;
 	fd = open(file, O_RDONLY);
-	unparsed_input = ft_strnew(0);
+	input = ft_strnew(0);
 	while (get_next_line(fd, &line) > 0)
-	{
-		x = ft_strlen(line);
-		unparsed_input = ft_strjoin(unparsed_input, line);
-	}
-	
-	while (unparsed_input[++i])
-	{
-		if (i % x == 0)
-		{
-
-		}
-	}
+		input = ft_strjoin(input, line);
 	close(fd);
+	return (input);
 }
 
 int	main(int ac, char **av)
 {
-	int		x;
-	int		y;
-	char	**input;
+	char	*input;
 
 	if (ac == 2)
 	{
-		input = parser(av[1], &input);
+		input = reader(av[1]);
+		ft_putstr(input);
 	}
 	else
 		ft_putstr("Usage: blah blah blah");
